de13789d3cbb6b6a9c791bb7260582d188249f41
viknash <viknash@hobbiz.com>
2016-11-29 18:52:14 +0800

    Compilation fix for array - 1st pass for multithreaded detector

--- include/utils.h
+++ include/utils.h
@@ -8,6 +8,8 @@
 #define DEBUGONLY(x)
 #endif
 
+#define unique_variable(basename) basename##__LINE__
+
 namespace task_scheduler {
 
     inline uint64_t CreateMask64()
@@ -20,5 +22,37 @@ namespace task_scheduler {
     {
         return CreateMask64(args...) | 1ull << first;
     }
+
+#if 0
+    class multi_threaded_detector
+    {
+
+    public:
+        inline multi_threaded_detector()
+        {
+            previous_thread_id = 0;
+        }
+
+        inline void enter()
+        {
+            int64_t current_thread_id = std::this_thread::get_id();
+            previous_thread_id = G4::Atomic::Exchange(detector.GetCurrentThreadId(), currentThread);
+            bool valid = m_PreviousThreadId == currentThread || m_PreviousThreadId == 0;
+            assert(valid);
+        }
+
+        inline void Exit(MultiThreadDetector& detector)
+        {
+            int64_t previousThreadId = exchange(detector.GetCurrentThreadId(), m_PreviousThreadId);
+            int64_t currentThread = std::this_thread::get_id();
+            bool valid = previousThreadId == currentThread || previousThreadId == 0;
+            assert!(valid);
+        }
+
+    private:
+
+        std::atomic_int64_t previous_thread_id;
+    };
+#endif
 
 };
